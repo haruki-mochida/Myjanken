fd157cf37ddda879d4f5bba28bf094f545797ebf
mochidaharuki <mochidaharuki@plise.co.jp>
2023-01-15 14:41:20 +0900

    Initial Commit

---
+++ Myjanken/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Myjanken
+//
+//  Created by 持田晴生 on 2023/01/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Myjanken/MyjankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyjankenApp.swift
+//  Myjanken
+//
+//  Created by 持田晴生 on 2023/01/15.
+//
+
+import SwiftUI
+
+@main
+struct MyjankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
